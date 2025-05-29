7b725bbb353828a7cae0b82257be46af5d090f00
marekszczepanik-git <github.markus@gmail.com>
2025-05-29 20:27:45 +0200

    Create server.js file

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 const express = require("express");
+const expressGraphQL = require("express-graphql").graphqlHTTP;
+
 const app = new express();
+
+app.use(
+  "/graphql",
+  expressGraphQL({
+    graphiql: true,
+  })
+);
 
 app.listen(4000, () => {
   console.log("Nasłuchiwanie");
